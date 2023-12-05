42adfb774affc043dc8cda8fe8eae24f0587ac4e
Tahir Shakir <tahir60652@gmail.com>
2023-12-05 15:55:49 +0500

    Add prompt component and landing route

---
+++ src/components/prompt.tsx
@@ -0,0 +1,9 @@
+// This is an input form for the user to enter a prompt for the AI to generate a response to.
+export default function Prompt() {
+  return (
+    <div>
+      <p>This is the prompt page</p>
+      <input type="text" placeholder="Enter a prompt" />
+    </div>
+  );
+}
