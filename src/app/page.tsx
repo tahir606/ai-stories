aa03336785c2ae6ff88c1e3758d6537ff16d5d38
Tahir Shakir <tahir60652@gmail.com>
2023-12-16 16:45:07 +0500

    Add redirect to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Prompt from "@/components/prompt";
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
-  );
+  // Redirect to the landing route
+  redirect("/landing");
 }
