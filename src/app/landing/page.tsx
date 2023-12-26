ccaf6e88aad7f82bf90a67beb454c73a9d179577
Tahir Shakir <tahir60652@gmail.com>
2023-12-26 16:31:00 +0500

    Add button to generate

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -3,9 +3,7 @@ import Prompt from "@/components/prompt";
 export default function Landing() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Prompt></Prompt>
-      </div>
+      <Prompt></Prompt>
     </main>
   );
 }
--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -1,9 +1,15 @@
 // This is an input form for the user to enter a prompt for the AI to generate a response to.
 export default function Prompt() {
   return (
-    <div>
-      <p>This is the prompt page</p>
-      <input type="text" placeholder="Enter a prompt" />
+    <div className="w-full">
+      <div>
+        <input className="w-full p-4" type="text" placeholder="Enter a prompt" />
+      </div>
+      <div className="text-center my-3">
+        <button className="bg-slate-700 hover:bg-black text-white font-bold py-4 px-8 rounded">
+          Generate
+        </button>
+      </div>
     </div>
   );
 }
