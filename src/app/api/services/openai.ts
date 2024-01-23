acd19c81edc4f2120f7ffc5e8374edfe39caccf0
Tahir Shakir <tahir60652@gmail.com>
2024-01-23 17:18:04 +0500

    Add story

--- src/app/api/services/openai.ts
+++ src/app/api/services/openai.ts
@@ -1,17 +1,17 @@
 import OpenAI from "openai";
 
-console.log(process.env.OPENAI_API_KEY);
-
-const openai = new OpenAI({
-  apiKey: "",
-  dangerouslyAllowBrowser: true,
-});
-
 export async function createAPI() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
+  console.log(process.env.OPENAI_API_KEY);
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
   });
 
-  console.log(completion.choices[0]);
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "Write me a story." }],
+    model: "gpt-3.5-turbo",
+    temperature: 1,
+    max_tokens: 128,
+  });
+
+  console.log(completion);
 }
